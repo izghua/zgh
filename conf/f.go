14086dbcab03518659fc40e279aef8238e9e286b
叶落山城 <zghua@zghua.local>
2018-12-02 01:36:08 +0800

    :egg: test

--- conf/f.go
+++ conf/f.go
@@ -1,40 +1,17 @@
 package conf
 
+//type Mysql interface {
+//	DbUser()
+//	DbHost()
+//	DbPort()
+//	DbDatabase()
+//	DbUserName()
+//	DBPassword()
+//}
 
-type Mysql interface {
-	DbUser()
-	DbHost()
-	DbPort()
-	DbDatabase()
-	DbUserName()
-	DBPassword()
+
+type F interface {
+
 }
 
-
-type f interface {
-	Mysql()
-}
-
-type Sql struct {
-}
-
-func (sql *Sql) DbUser()  {
-}
-
-func (sql *Sql) DbHost() string {
-	return DBHOST
-}
-
-func (sql *Sql) DbPort() string {
-	return DBPORT
-}
-
-func (sql *Sql) DbDatabase() {
-}
-
-func (sql *Sql) DbUserName() {
-}
-
-func (sql *Sql) DBPassword() {
-}
 
--- conn/mysql.go
+++ conn/mysql.go
@@ -6,20 +6,64 @@
  */
 package conn
 
-import "izghua/pkg/zgh/conf"
+import (
+	"fmt"
+	"github.com/go-xorm/xorm"
+	"gitlab.yixinonline.org/pkg/bang/conf"
+	"gitlab.yixinonline.org/pkg/bang/util"
+	"time"
+)
 
-//type GetMySql struct {
-//
-//}
-//
-//
-//func (mysql *GetMySql) f() {
-//
-//}
+var mysql *xorm.Engine
 
-
-func GetMySql() {
-	var mysql conf.Mysql
-	
+type SqlParam struct {
+	User string
+	Host string
+	Port string
+	DataBase string
+	UserName string
+	Password string
 }
+
+func InitMysql(s *SqlParam) *xorm.Engine {
+	dataSourceName := s.User + ":" + s.Password + "@/" + s.DataBase + "?charset=utf8"
+	engine, err := xorm.NewEngine("mysql", dataSourceName)
+	if err != nil {
+		panic("初始化数据库，创建连接异常:" + err.Error())
+	}
+	engine.TZLocation = conf.TimeLocation
+	engine.SetMaxIdleConns(3)
+	engine.SetMaxOpenConns(20)
+	engine.SetConnMaxLifetime(0)
+	engine.ShowExecTime(true)
+	engine.ShowSQL(true)
+	mysql = engine
+	timer := time.NewTicker(time.Minute * 10)
+	go func(conn *xorm.Engine) {
+		for _ = range timer.C {
+			if err = mysql.Ping(); err != nil {
+				MySQLAutoConnect()
+			}
+		}
+	}(mysql)
+	return mysql
+}
+
+func autoConnectMySQL(tryTimes int, maxTryTimes int) int {
+	tryTimes++
+	if tryTimes <= maxTryTimes {
+		if mysql.Ping() != nil {
+			message := fmt.Sprintf("数据库连接失败,已重连%d次", tryTimes)
+			//yrdLog.GetLogger().Error("mysql", message)
+			go util.Alarm(message, util.ALARMALERT)
+		}
+		tryTimes = autoConnectMySQL(tryTimes, maxTryTimes)
+	}
+	return tryTimes
+}
+
+func MySQLAutoConnect() {
+	autoConnectMySQL(0, 5)
+}
+
 
