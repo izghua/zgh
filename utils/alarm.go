00d4b89fb3237b9d87c9cda2b13d1fe43712f89a
yeluo <2067930913@qq.com>
2018-12-11 23:17:46 +0800

    :egg: edit something

--- utils/alarm.go
+++ utils/alarm.go
@@ -7,6 +7,8 @@
 package utils
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -14,16 +16,20 @@ import (
 // for to check the params is right
 type AlarmType string
 
+type AlarmMailReceive string
+
 // this are some const params what i defined
 // only this can be to input
 const  (
 	AlarmTypeOne AlarmType = "mail"
 	AlarmTypeTwo AlarmType = "wechat"
 	AlarmTypeThree AlarmType = "message"
+	//AlarmMailTo AlarmMailReceive = ""
 )
 
 type AlarmParam struct {
 	Types AlarmType
+	MailTo AlarmMailReceive
 }
 
 var alarmParam *AlarmParam
@@ -48,6 +54,32 @@ func (alarm *AlarmParam)SetType(t AlarmType) ap {
 		return ty
 	}
 }
+
+func (alarm *AlarmParam)SetMailTo(t AlarmMailReceive) ap {
+	return func(alarm *AlarmParam) interface{} {
+		to := alarm.MailTo
+		t.CheckIsNull().MustMailFormat()
+		alarm.MailTo = t
+		return to
+	}
+}
+
+// alarm receive account can not null
+func (t AlarmMailReceive)CheckIsNull() AlarmMailReceive {
+	if len(t) == 0 {
+		panic("不能为空")
+	}
+	return t
+}
+
+// alarm receive account must be mail format
+func (t AlarmMailReceive)MustMailFormat() AlarmMailReceive {
+	if m, _ := regexp.MatchString("^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+", string(t)); !m {
+		panic("格式不正确")
+	}
+	return t
+}
+
 
 // judge it is a right type what i need
 // if is it a wrong type, i must return a panic to above
@@ -80,6 +112,26 @@ func (alarm *AlarmParam)AlarmInit(options ...ap) *AlarmParam {
 
 
 func Alarm(content string,priority string) {
+	fmt.Println(alarmParam.MailTo,alarmParam.MailTo == "")
+	types := strings.Split(string(alarmParam.Types),",")
+	var err error
+	for _,a := range types {
+		switch AlarmType(a) {
+		case AlarmTypeOne:
+			if alarmParam.MailTo == "" {
+				panic("邮件接收者不能为空")
+			}
+			err = SendMail(string(alarmParam.MailTo),"报警",content)
+			break
+		case AlarmTypeTwo:
+			break
+		case AlarmTypeThree:
+			break
+		}
+		if err != nil {
+			panic(err.Error())
+		}
+	}
 
-
+	fmt.Println(alarmParam.Types,"baojing",alarmParam.MailTo)
 }
