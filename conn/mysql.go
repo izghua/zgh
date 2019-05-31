cf568050769fedb3f4140009e88fed4f9d908d48
yela <2067930913@qq.com>
2019-05-31 11:30:31 +0800

    :apple: mysql log

--- conn/mysql.go
+++ conn/mysql.go
@@ -127,6 +127,7 @@ func MySQLAutoConnect() {
 
 
 func SqlDump(fileName string,filePath string) error {
+	zgh.ZLog().Info("message","sql dump file","name",fileName,"path",filePath)
 	err := os.Remove(fileName)
 	if err != nil {
 		zgh.ZLog().Error("message","sql dump has error","error",err.Error())
